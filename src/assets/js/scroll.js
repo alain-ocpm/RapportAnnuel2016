9c816807f6484569179239e403e61d2f392350c4
alain-ocpm <alain.benoit@ocpm.qc.ca>
2017-04-12 14:42:32 -0400

    GreenSock animations

--- src/assets/js/scroll.js
+++ src/assets/js/scroll.js
@@ -7,20 +7,32 @@ if (/Mobi/.test(navigator.userAgent)) {
 else {
   // Anything that isn't considered a mobile device
 
+  //General
+  $(".text-anim-fadeup").each(function() {
+    var tween = TweenMax.from($(this), 0.3, {autoAlpha: 0, scale: 0.8, y: "+=15", ease:Power1.easeOut});
+    var scene = new ScrollMagic.Scene({triggerElement: this})
+                  .setTween(tween)
+                  .addTo(controller);
+  });
+
   //Welcome
   new ScrollMagic.Scene({triggerElement: "#welcome"})
-    .setClassToggle("#welcome h1", "anim")
+    .setTween(TweenMax.from("#welcome", 0.3, {autoAlpha: 0, ease:Linear.easeNone}))
+    .addTo(controller);
+
+  new ScrollMagic.Scene({triggerElement: "#welcome"})
+    .setTween(TweenMax.from("#welcome h1", 0.5, {autoAlpha: 0, x: "-=100", ease:Power1.easeOut}))
     .offset(100)
     .addTo(controller);
 
   //President
   new ScrollMagic.Scene({triggerElement: "#president"})
-    .setClassToggle("#president-bubble01", "anim")
-    .offset(250)
+    .setTween(TweenMax.from("#president-bubble01", 0.5, {autoAlpha: 0, y: "+=100", ease:Power1.easeOut}))
+    .offset(200)
     .addTo(controller);
 
   new ScrollMagic.Scene({triggerElement: "#president"})
-    .setClassToggle("#president h1", "anim")
+    .setTween(TweenMax.from("#president h1", 0.5, {autoAlpha: 0, x: "-=100", ease:Power1.easeOut}))
     .offset(250)
     .addTo(controller);
 }
